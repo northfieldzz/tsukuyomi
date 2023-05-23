32e7e45da113bc5104d47492cf97ff4252c465dc
northfieldzz <northfield.t@outlook.com>
2023-05-24 08:57:09 +0900

    Update: wsgi

--- tsukuyomi/__init__.py
+++ tsukuyomi/__init__.py
@@ -1,15 +1,16 @@
+from os import environ
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
 
-    from webapp.v1 import v1_bp
+    from tsukuyomi.v1 import v1_bp
 
     app.register_blueprint(v1_bp, url_prefix="/v1")
 
     @app.route('/version')
     def version():
-        return '0.0.0'
+        return environ.get('VERSION')
 
     return app
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from tsukuyomi import create_app
+from waitress import serve
+
+if __name__ == '__main__':
+    app = create_app()
+    serve(app, host='0.0.0.0', port=80)
