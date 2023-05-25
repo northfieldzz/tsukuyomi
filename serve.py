2692660e5eca98d76ef440b431ef6162ba4c1519
RikuGoto <riku.goto@email.com>
2023-05-26 00:39:13 +0900

    chinko

--- serve.py
+++ serve.py
@@ -1,5 +1,5 @@
 from tsukuyomi import create_app
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host='0.0.0.0', debug=True)
+    create_app()
+   # app.run(host='0.0.0.0', debug=True)
